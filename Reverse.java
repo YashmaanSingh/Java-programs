973deee94f281059e6029cfed7d571c592d354b9
YashmaanSingh <156333050+YashmaanSingh@users.noreply.github.com>
2025-06-05 23:59:59 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -5,6 +5,15 @@ public class Reverse{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Enter the number you want to Reverse: ");
+        int num = sc.nextInt();
+        int rev = 0;
+
+        while(num !=0){
+        int a = num % 10;
+        rev = rev *10 + a;
+        num = num / 10;
+    }
+    System.out.print("The reverse number is: "+ rev);
     }
 }
